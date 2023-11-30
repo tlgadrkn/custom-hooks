44cc6fa3edb458e0ce9e9f87529f22461a948975
Tolga Durukan <tolgadurukan16@gmail.com>
2023-11-30 10:35:07 +0200

    init

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import './index.css';
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+import App from './App';
+
+const root = createRoot(document.getElementById('root')!);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
